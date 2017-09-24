0a49c6323ce4a140047498b004fc3d112cbdac6a
alejacadavid <mariaa.cadavid@udea.edu.co>
2017-09-24 18:56:00 -0500

    Conexión a base de datos Heroku

--- src/main/java/org/tourofheroes/repositorio/TourofHeroesRepository.java
+++ src/main/java/org/tourofheroes/repositorio/TourofHeroesRepository.java
@@ -1,11 +1,17 @@
 package org.tourofheroes.repositorio;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.tourofheroes.dominio.Hero;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface TourofHeroesRepository{
+public interface TourofHeroesRepository extends JpaRepository<Hero, String> {
+	
+	public Optional<Hero> findById(int id);
 
-	public List<Hero> getHeroes();
+	public List<Hero> removeById();
+
+	public List<Hero> findAll();
 
 }
--- src/main/java/org/tourofheroes/services/rest/TourofHeroesRestController.java
+++ src/main/java/org/tourofheroes/services/rest/TourofHeroesRestController.java
@@ -1,12 +1,14 @@
 package org.tourofheroes.services.rest;
 
 import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.tourofheroes.dominio.Hero;
-import org.tourofheroes.repositorio.TourofHeroesRepositorioImpl;
+import org.tourofheroes.exception.DataNotFound;
 import org.tourofheroes.repositorio.TourofHeroesRepository;
 
 @RestController
@@ -14,26 +16,35 @@ import org.tourofheroes.repositorio.TourofHeroesRepository;
 public class TourofHeroesRestController {
 	private TourofHeroesRepository tourofHeroesRepository;
 	
-	public TourofHeroesRestController(){
-		this.tourofHeroesRepository = new TourofHeroesRepositorioImpl();
+	
+	
+	
+
+	public TourofHeroesRestController(TourofHeroesRepository tourofHeroesRepository){
+		super();
+		this.tourofHeroesRepository = tourofHeroesRepository;
 	}
 	
+	
 	@RequestMapping("listar")
-	@ResponseBody
-	  public List<Hero> getHeroes() {
-		return tourofHeroesRepository.getHeroes();
-	  }
+	public List<Hero> getHeroes(){
+		return tourofHeroesRepository.findAll();
+	}
 	
 	@RequestMapping("consultar")
 	public Hero getHero(int id) throws DataNotFound{
-		log.info("Entro a consultar");
+		
 		Optional<Hero> hero = tourofHeroesRepository.findById(id);
 		if(!hero.isPresent()){
 			throw new DataNotFound("No existe hero");
 		}
 		return hero.get();
 	}
-
+	
+	@RequestMapping("borrar")
+	public List<Hero> removeById(int id){
+		return tourofHeroesRepository.removeById();
+	}
 }
 
 
