675b14a272b4384646034f8f0a299ad5951cc401
alejacadavid <mariaa.cadavid@udea.edu.co>
2017-09-24 12:34:21 -0500

    REST API - Tour of Heroes

--- src/main/java/org/tourofheroes/Application.java
+++ src/main/java/org/tourofheroes/Application.java
@@ -2,12 +2,19 @@ package org.tourofheroes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication 
-public class Application { 	
-	
-	// para correc con maven: mvn spring-boot:run
+@SpringBootApplication
+@ComponentScan(basePackages="org.tourofheroes")
+@EntityScan(basePackages = { "org.tourofheroes.dominio" }, basePackageClasses = { Jsr310JpaConverters.class })
+@EnableJpaRepositories(basePackages = { "org.tourofheroes.repositorio" })
+
+public class Application {
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args); 
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/main/java/org/tourofheroes/services/rest/TourofHeroesRestController.java
+++ src/main/java/org/tourofheroes/services/rest/TourofHeroesRestController.java
@@ -1,17 +1,39 @@
 package org.tourofheroes.services.rest;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.tourofheroes.dominio.Hero;
+import org.tourofheroes.repositorio.TourofHeroesRepositorioImpl;
+import org.tourofheroes.repositorio.TourofHeroesRepository;
 
 @RestController
 @RequestMapping("/tourofheroes")
 public class TourofHeroesRestController {
+	private TourofHeroesRepository tourofHeroesRepository;
 	
-	@RequestMapping("/index")
-	  public String index() {
-	    return "Hola mundo ";
+	public TourofHeroesRestController(){
+		this.tourofHeroesRepository = new TourofHeroesRepositorioImpl();
+	}
+	
+	@RequestMapping("listar")
+	@ResponseBody
+	  public List<Hero> getHeroes() {
+		return tourofHeroesRepository.getHeroes();
 	  }
+	
+	@RequestMapping("consultar")
+	public Hero getHero(int id) throws DataNotFound{
+		log.info("Entro a consultar");
+		Optional<Hero> hero = tourofHeroesRepository.findById(id);
+		if(!hero.isPresent()){
+			throw new DataNotFound("No existe hero");
+		}
+		return hero.get();
+	}
+
 }
 
 
